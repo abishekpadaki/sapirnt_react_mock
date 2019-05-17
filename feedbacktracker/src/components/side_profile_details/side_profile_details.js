98d14da143c51fbf8b7667b74e8194a3b798e2a0
Abishek Padaki <abiks192@gmail.com>
2019-05-17 08:15:42 +0530

    Added Side profile component

---
+++ feedbacktracker/src/components/side_profile_details/side_profile_details.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+
+export class SideProfile extends React.Component{
+    render() {
+        return (
+            
+            <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="#" />
+  <Card.Body>
+    
+    <Card.Text>
+      <h2>{/*Name*/}</h2>
+      <h3>{/*Oracle ID*/}</h3>
+      <h3>{/*Designation*/}</h3>
+    </Card.Text>
+  
+  </Card.Body>
+</Card>
+)}
+        }
