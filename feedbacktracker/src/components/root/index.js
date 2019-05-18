d759105aac0ea58cb985dece73c8d8b57ce65cae
Abishek Padaki <abiks192@gmail.com>
2019-05-18 19:30:14 +0530

    Added Feedback history file (not completed)

--- feedbacktracker/src/components/root/index.js
+++ feedbacktracker/src/components/root/index.js
@@ -3,6 +3,7 @@ import { Route,Switch} from 'react-router-dom';
 import { Login } from "../../views/LoginView/index";
 import { Dashboard } from "../../views/Dashboard/index";
 import { FeedbackForm } from "../../views/FeedbackForm/index";
+import { FeedbackHistory } from '../../views/FeedbackHistory';
 
 export default class RootApp extends React.Component{
     render(){
@@ -11,6 +12,7 @@ export default class RootApp extends React.Component{
                           <Route exact path='/' render={()=><Login {...this.props}/>} />
                           <Route exact path='/dashboard' render={()=> <Dashboard {...this.props} />} />
                           <Route exact path='/feedback_form' render={()=> <FeedbackForm {...this.props} />} />
+                          <Route exact path='/feedback_history' render={()=> <FeedbackHistory {...this.props} />} />
                         </Switch>
                     </div>
     }
