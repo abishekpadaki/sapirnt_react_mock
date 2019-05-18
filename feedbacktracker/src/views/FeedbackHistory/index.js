6f724e8ed3ca1f240e87dfd8db0bc16754dc1412
LochanRn <rajanaga_ra@srmuniv.edu.in>
2019-05-18 20:40:53 +0530

    completed feedbackHistory :)

--- feedbacktracker/src/views/FeedbackHistory/index.js
+++ feedbacktracker/src/views/FeedbackHistory/index.js
@@ -5,13 +5,23 @@ import {NavbarComponent} from '../../components/navbar/index'
 export class FeedbackHistory extends React.Component{
     constructor(props){
         super(props);
+        this.state = {
+            header:["Id", "FeedBack", "Rating"],
+            values:[
+                {
+                    Id:"123456",
+                    FeedBack:"Amazing work keep going.",
+                    OverAllRating:"4"
+                }
+            ]
+        }
     }
 
     render(){
         return(
             <React.Fragment>
                 <NavbarComponent/>
-                <Tbl/>
+                <Tbl header={this.state.header} values={this.state.values} />
             </React.Fragment>
         )
     }
